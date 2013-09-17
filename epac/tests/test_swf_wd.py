adca4533931d233c9ac4d745bad28f21cd54a9de
jinpeng <mr.li.jinpeng@gmail.com>
2013-09-17 21:49:35 +0200

    ENH: uni test for working directory of soma-workflow

--- epac/tests/test_swf_wd.py
+++ epac/tests/test_swf_wd.py
@@ -10,19 +10,45 @@ import os
 from soma_workflow.client import Job, Workflow
 from soma_workflow.client import Helper, FileTransfer
 from soma_workflow.client import WorkflowController
+import soma_workflow.constants as constants
 import socket
 import os.path
+from distutils.version import LooseVersion as V
+import dill
+import joblib
 
 
 if __name__ == '__main__':
+
+    if V(dill.__version__) < V("0.2a"):
+        raise ValueError("dill version is too old to use, please use version "\
+                "on https://github.com/uqfoundation")
+    if V(joblib.__version__) < V("0.7.1"):
+        raise ValueError("joblib version is too old to use, please use version "\
+                "on https://github.com/joblib/joblib")
     tmp_work_dir_path = tempfile.mkdtemp()
     cur_work_dir = os.getcwd()
+    cur_file_dir = os.path.dirname(os.path.abspath(__file__))
     test_filepath = u"./onlytest.txt"
+    test_bash_script = u"./testbs.sh"
 
-    job = Job(command=[u"touch", test_filepath],
+    os.chdir(tmp_work_dir_path)
+    fileout = open(test_bash_script, "w+")
+    filecontent = """#!/bin/bash
+echo %s
+""" % test_bash_script
+    fileout.write(filecontent)
+    fileout.close()
+    os.chdir(cur_work_dir)
+
+    job1 = Job(command=[u"touch", test_filepath],
                     name="epac_job_test",
                     working_directory=tmp_work_dir_path)
-    soma_workflow = Workflow(jobs=[job])
+    job2 = Job(command=["%s/readfile" % cur_file_dir, test_bash_script],
+                    name="epac_job_test",
+                    working_directory=tmp_work_dir_path)
+
+    soma_workflow = Workflow(jobs=[job1, job2])
 
     resource_id = socket.gethostname()
     controller = WorkflowController(resource_id, "", "")
@@ -31,7 +57,11 @@ if __name__ == '__main__':
     wf_id = controller.submit_workflow(workflow=soma_workflow,
                                        name="epac workflow")
     Helper.wait_workflow(wf_id, controller)
+    nb_failed_jobs = len(Helper.list_failed_jobs(wf_id, controller))
+    if nb_failed_jobs > 0:
+        raise ValueError("Soma-workflow error, cannot use working directory")
+    
     if not os.path.isfile(os.path.join(tmp_work_dir_path, test_filepath)):
         raise ValueError("Soma-workflow cannot define working directory")
     else:
-        print "OK"
+        print "OK for creating new file in working directory"
