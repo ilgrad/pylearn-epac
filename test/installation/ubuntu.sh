007db2d0c5e7d08d3720bd315007a701ad3281dd
LaureHugo <hugolaure@free.fr>
2013-09-19 14:20:51 +0200

    DOC: correcting installation steps

--- test/installation/ubuntu.sh
+++ test/installation/ubuntu.sh
@@ -13,8 +13,8 @@ sudo apt-get install python-pip
 
 sudo apt-get install python-setuptools python-dev build-essential libatlas-dev python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
 
-pip install --user scikit-learn
-pip install --user soma-workflow
+sudo pip install scikit-learn
+sudo pip install soma-workflow
 
-python setup.py install --user
+sudo python setup.py install
 
