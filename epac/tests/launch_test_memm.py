7ea93dff36d6a52590cc842ae2f0fe12806276f8
LaureHugo <hugolaure@free.fr>
2013-09-27 15:01:37 +0200

    ENH: improve memory testing

--- epac/tests/launch_test_memm.py
+++ epac/tests/launch_test_memm.py
@@ -7,10 +7,9 @@ Created on Thu Sep 26 12:02:00 2013
 
 import os
 import time
-import psutil
-import subprocess
 import commands
 import datetime
+
 
 def get_pid(process_name):
     cmd = 'ps -ef | '\
@@ -35,6 +34,7 @@ def get_mem_cost(process_name):
 
 n_samples = 50
 test_list = [500, 1000]
+memmap = 'True'
 
 filename = 'result_test.txt'
 
@@ -45,12 +45,13 @@ open(filename, 'w').close()
 #test_list = [50000 * 2**n for n in range(0, 10)]
 for n_features in test_list:
     process_name = "test_memmapping.py"
-    os.system("(unbuffer python %s %i %i \
-               >> %s &)" %(process_name, n_samples, n_features, filename))
+    os.system("(unbuffer python %s %i %i %s \
+               >> %s &)" %(process_name, n_samples, n_features, memmap, filename))
     time.sleep(2)
-    
+
     print "=========================="
     print "n_features = ", n_features
+    print "memmap enabled = ", memmap
     start_time = datetime.datetime.now()
     print "Starting time = ", repr(start_time)
     max_mem_cost = 0
@@ -63,10 +64,10 @@ for n_features in test_list:
             max_mem_cost = mem_cost
         time.sleep(10)
         print "memory cost = ", mem_cost
-    print "max memory cost = ", max_mem_cost 
+    print "max memory cost = ", max_mem_cost
     finished_time = datetime.datetime.now()
     print "Finished time = ", repr(finished_time)
     print "Time cost=", repr((finished_time - start_time).seconds)
-    
+
     with open(filename, "a") as myfile:
         myfile.write("\n \n")
--- epac/tests/test_memmapping.py
+++ epac/tests/test_memmapping.py
@@ -5,16 +5,14 @@ Created on Wed Sep 25 15:56:37 2013
 @author: laure
 """
 
-from memory_profiler import profile
 import numpy as np
-import os.path as path
-from tempfile import mkdtemp
 from sklearn import datasets
 import random
 import datetime
 import sys
 import os
 import tempfile
+
 
 def create_mmat(nrows, ncols, default_values=None):
     now = datetime.datetime.now()
@@ -43,19 +41,9 @@ def create_array(size, default_values=None):
     ret_array = ret_array[:, 0]
     return ret_array
 
-#
-#def convert2memmap(np_mat):
-#    filename = path.join(mkdtemp(), 'newfile.dat')
-#    mem_mat = np.memmap(filename,\
-#                     dtype='float32',\
-#                     mode='w+',\
-#                     shape=np_mat.shape)
-#    mem_mat[:] = np_mat[:]
-#    return mem_mat
-
 
 # @profile
-def func_memm_local(n_samples, n_features):
+def func_memm_local(n_samples, n_features, memmap):
     ''' Test the capacity of the computer
 
     Parameters
@@ -63,74 +51,53 @@ def func_memm_local(n_samples, n_features):
     n_samples: number of rows of the X matrix
 
     n_features: number of columns of th X matrix
+
+    memmap: if True, use memory mapping to reduce memory cost
     '''
-    print " ------- memm_local pt1 : beginning with", n_features, "features -------"
-    ## 1) Build a np.memmap dataset for big matrix
+
+    ## 1) Building dataset
     ## ============================================================
-    X = create_mmat(n_samples, n_features)
-    y = create_array(n_samples, [0,1])
+    print " -> Pt1 : Beginning with", n_features, "features, memmap =", memmap
+    if memmap:
+        X = create_mmat(n_samples, n_features)
+        y = create_array(n_samples, [0, 1])
 
-    print "X matrix file size =", os.path.getsize(X.filename), "bytes"
-    print "y vector file size =", os.path.getsize(y.filename), "bytes"
+        print "X matrix file size =", os.path.getsize(X.filename), "bytes"
 
-    Xy = dict(X=X, y=y)
+        Xy = dict(X=X, y=y)
+    else:
+
+        X, y = datasets.make_classification(n_samples=n_samples,
+                                            n_features=n_features,
+                                            n_informative=2,
+                                            random_state=1)
+    
+        Xy = dict(X=X, y=y)
     ## 2) Build two workflows respectively
     ## =======================================================
-    print " -> memm_local pt2 : X and y created, building workflow"
+    print " -> Pt2 : X and y created, building workflow"
     from sklearn.svm import SVC
     from epac import CV, Methods
     cv_svm_local = CV(Methods(*[SVC(kernel="linear"),
                           SVC(kernel="rbf")]),
                           n_folds=3)
-    print " -> memm_local pt3 : Workflow built, running"
-    cv_svm_local.run(**Xy)
-#    from epac import LocalEngine
-#    local_engine = LocalEngine(cv_svm_local, num_processes=1)
-#    cv_svm = local_engine.run(**Xy)
-    print " -> memm_local pt4 : Finished running, reducing"
-    cv_svm_local.reduce()
-    print " -> memm_local pt5 : finished with", n_features, "features"
+    print " -> Pt3 : Workflow built, running"
 
+#    # Single process on local engine
+#    cv_svm_local.run(**Xy)
+#    print " -> Pt4 : Finished running single-process, reducing"
+#    cv_svm_local.reduce()
 
-# @profile
-def func_no_memm_local(n_samples, n_features):
-    ''' Test the capacity of the computer
+    # Multiple processes on local engine
+    from epac import LocalEngine
+    local_engine = LocalEngine(cv_svm_local, num_processes=1)
+    cv_svm = local_engine.run(**Xy)
+    print " -> Pt4 : Finished running multi-processes, reducing"
+    cv_svm.reduce()
 
-    Parameters
-    ----------
-    n_samples: number of rows of the X matrix
-
-    n_features: number of columns of th X matrix
-    '''
-    print " ------- no_memm_local pt1 : beginning with", n_features, "features -------"
-    ## 1) Build a np.memmap dataset for big matrix
-    ## ============================================================
-    X, y = datasets.make_classification(n_samples=n_samples,
-                                        n_features=n_features,
-                                        n_informative=2,
-                                        random_state=1)
-    print "X matrix file size =", sys.getsizeof(X), "bytes"
-
-    Xy = dict(X=X, y=y)
-    ## 2) Build two workflows respectively
-    ## =======================================================
-    print " -> no_memm_local pt2 : X and y created, building workflow"
-    from sklearn.svm import SVC
-    from epac import CV, Methods
-    cv_svm_local = CV(Methods(*[SVC(kernel="linear"),
-                          SVC(kernel="rbf")]),
-                          n_folds=3)
-    print " -> no_memm_local pt3 : Workflow built, running"
-    cv_svm_local.run(**Xy)
-#    from epac import LocalEngine
-#    local_engine = LocalEngine(cv_svm_local, num_processes=1)
-#    cv_svm = local_engine.run(**Xy)
-    print " -> no_memm_local pt4 : Finished running, reducing"
-    cv_svm_local.reduce()
-    print " -> no_memm_local pt5 : finished with", n_features, "features"
+    print " -> Pt5 : Finished with", n_features, "features"
 
 
 if __name__ == "__main__":
     args = sys.argv[1:]
-    args = [50, 100]
-    func_memm_local(int(args[0]), int(args[1]))
+    func_memm_local(int(args[0]), int(args[1]), args[2])
