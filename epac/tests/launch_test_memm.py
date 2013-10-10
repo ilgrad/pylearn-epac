a8ce920c5af0140f2f68aa8531afc0509f2557c0
LaureHugo <hugolaure@free.fr>
2013-10-10 15:18:24 +0200

    TEST: improving memory testing with job info from the cluster

--- epac/tests/launch_test_memm.py
+++ epac/tests/launch_test_memm.py
@@ -33,7 +33,7 @@ def get_pid(process_name):
 
 def get_mem_cost(process_name):
     ''' Return the sum of the memory costs of all processes whose name
-        contains process_name.
+        contain process_name.
     '''
     list_pid = get_pid(process_name)
     if not list_pid:
@@ -49,9 +49,9 @@ def get_mem_cost(process_name):
 
 def print_process_mem_cost(process_name, delay=10):
     '''  Print the memory cost of all processes whose name contains
-        process_name.
+    process_name.
 
-    Print every 'delay' secondes the memory cost of all the processes
+    Measure every 'delay' seconds the memory cost of all the processes
     whose name contains process_name, while at least one exists.
     Print the max memory cost at the end of the processes.
     '''
@@ -64,71 +64,88 @@ def print_process_mem_cost(process_name, delay=10):
         if max_mem_cost < mem_cost:
             max_mem_cost = mem_cost
         time.sleep(delay)
-        print "memory cost = ", mem_cost
+        # print "memory cost = ", mem_cost
     print "max memory cost = ", max_mem_cost
 
+
+###################################
 
 #################################
 ## Definition of the variables ##
 #################################
 
 n_samples = 500
-n_features_list = [50000 * (2 ** n) for n in range(0, 6)]
-memmap = True
+n_features_list = [50000 * 2 ** n for n in range(0, 7)]
+#n_features_list = [10000]
+list_memmap = [False, True]
 n_proc_list = range(1, 9)
-is_on_cluster = False
+cluster_list = [False, True]
 directory = '/volatile'
 
 # Path of the file to write results
 filename = 'result_test.txt'
 
 # Clearing file from other tries
-open(filename, 'w').close()
+open(filename, 'w+').close()
 
 
 ####################
 ## Memory testing ##
 ####################
 
-for n_proc in n_proc_list:
-    print "==========================="
-    print "====== %i process(es) ======" % n_proc
-    for n_features in n_features_list:
-        print "---------------------------"
-        print "n_features = ", n_features
-        print "memmap enabled = ", memmap
-        print "number of processes =", n_proc
-        process_name = "test_memmapping.py"
-        start_time = datetime.datetime.now()
-        print "Starting time = ", repr(start_time)
+# Clean previous results
+os.system('rm -rf /tmp/tmp*')
+if directory:
+    os.system('rm -rf %s/tmp*' % directory)
 
-        cmd = ""
-        if not is_on_cluster:
-            cmd = "(unbuffer python %s %i %i %s %i %s %s >> %s &)" % \
-                  (process_name,
-                   n_samples,
-                   n_features,
-                   repr(memmap),
-                   n_proc,
-                   repr(is_on_cluster),
-                   directory,
-                   filename)
-            os.system(cmd)
-            time.sleep(5)
-            print_process_mem_cost(process_name)
-        else:
-            cmd = "unbuffer python %s %i %i %s %i %s >> %s" % \
-                  (process_name,
-                   n_samples,
-                   n_features,
-                   memmap,
-                   n_proc,
-                   repr(is_on_cluster),
-                   filename)
-            os.system(cmd)
-        finished_time = datetime.datetime.now()
-        print "Finished time = ", repr(finished_time)
-        print "Time cost=", repr((finished_time - start_time).seconds)
+for memmap in list_memmap:
+    for n_features in n_features_list:
+        print "\n"
+        print "==========================="
+        print "------ %i features -------" % n_features
+        for is_on_cluster in cluster_list:
+            for n_proc in n_proc_list:
+                print "======= New try ============"
+                print "n_features = ", n_features
+                print "memmap enabled = ", memmap
+                print "number of processes =", n_proc
+                print "running on cluster =", is_on_cluster
+                process_name = "test_memmapping.py"
+                start_time = datetime.datetime.now()
+#                print "Starting time = ", repr(start_time)
+
+                cmd = ""
+                if not is_on_cluster:
+                    cmd = "(unbuffer python %s %i %i %s %i %s %s >> %s &)" % \
+                          (process_name,
+                           n_samples,
+                           n_features,
+                           repr(memmap),
+                           n_proc,
+                           repr(is_on_cluster),
+                           directory,
+                           filename)
+                    os.system(cmd)
+                    time.sleep(5)
+                    print_process_mem_cost(process_name, 5)
+                else:
+                    cmd = "unbuffer python %s %i %i %s %i %s >> %s" % \
+                          (process_name,
+                           n_samples,
+                           n_features,
+                           memmap,
+                           n_proc,
+                           repr(is_on_cluster),
+                           filename)
+                    os.system(cmd)
+                finished_time = datetime.datetime.now()
+#                print "Finishing time = ", repr(finished_time)
+                print "Time cost=", repr((finished_time - start_time).seconds)
+                print "------------- Results ------------------"
+                with open(filename, 'r+') as results:
+                    print results.read()
+                # Clear file from this try
+                open(filename, 'w+').close()
 
         # Remove the previously created temp files
         os.system('rm -rf /tmp/tmp*')
--- epac/tests/test_memmapping.py
+++ epac/tests/test_memmapping.py
@@ -8,44 +8,58 @@ Created on Wed Sep 25 15:56:37 2013
 import numpy as np
 from sklearn import datasets
 import random
-import datetime
+import time
+#import datetime
 import sys
 import os
-import tempfile
+#import tempfile
+import unittest
+import dill as pickle
+#import getopt
+from epac import StoreFs
+
+from epac.tests.utils import isequal, compare_two_node
 
 
-def create_mmat(nrows, ncols, default_values=None, dir=None):
-    now = datetime.datetime.now()
-    np.random.seed(now.second)
-    hfile = tempfile.NamedTemporaryFile("w+", dir=dir)
-    filename = hfile.name
-    print "Temporary path :", filename
-    hfile.close()
-    mem_mat = np.memmap(filename,
-                        dtype='float32',
-                        mode='w+',
-                        shape=(nrows, ncols))
-    for i in xrange(nrows):
-        if not default_values:
-            mem_mat[i, :] = np.random.random(size=ncols)
-        elif type(default_values) is list:
-            insert_row = np.zeros(ncols)
-            for j in xrange(len(insert_row)):
-                pos = random.randint(0, len(default_values) - 1)
-                insert_row[j] = default_values[pos]
-            mem_mat[i, :] = insert_row
+def create_mmat(nrows, ncols, default_values=None, dir=None, n_proc=None):
+    filename = 'tmp_rows_' + str(nrows) + '_cols_' + str(ncols)
+    if dir is None:
+        filepath = '/tmp/' + filename
+    else:
+        if not os.path.isdir(dir):
+            os.mkdir(dir)
+        filepath = os.path.join(dir, filename)
+
+    if n_proc == 1:
+        mem_mat = np.memmap(filepath,
+                            dtype='float32',
+                            mode='w+',
+                            shape=(nrows, ncols))
+        for i in xrange(nrows):
+            if not default_values:
+                mem_mat[i, :] = np.random.random(size=ncols)
+            elif type(default_values) is list:
+                insert_row = np.zeros(ncols)
+                for j in xrange(len(insert_row)):
+                    pos = random.randint(0, len(default_values) - 1)
+                    insert_row[j] = default_values[pos]
+                mem_mat[i, :] = insert_row
+    else:
+        mem_mat = np.memmap(filepath,
+                            dtype='float32',
+                            mode='r+',
+                            shape=(nrows, ncols))
     return mem_mat
 
 
-def create_array(size, default_values=None, dir=None):
-    ret_array = create_mmat(size, 1, default_values, dir=dir)
+def create_array(size, default_values=None, dir=None, n_proc=None):
+    ret_array = create_mmat(size, 1, default_values, dir=dir, n_proc=n_proc)
     ret_array = ret_array[:, 0]
     return ret_array
 
 
 # @profile
-def func_memm_local(n_samples, n_features, memmap=False,
-                    n_proc=1, is_swf=False, dir=None):
+class TestMemMapping(unittest.TestCase):
     ''' Test the capacity of the computer
 
     Parameters
@@ -71,75 +85,140 @@ def func_memm_local(n_samples, n_features, memmap=False,
         If None, save in /tmp
     '''
 
-    ## 1) Building dataset
-    ## ============================================================
-    print "\n \n"
-    print " -> Pt1 : Beginning with", n_features, "features, memmap =",\
-        memmap, ",", n_proc, "processes", "is_swf = ", is_swf
-    if memmap:
-        X = create_mmat(n_samples, n_features, dir=dir)
-        y = create_array(n_samples, [0, 1], dir=dir)
+    def __init__(self, n_samples, n_features, memmap=False,
+                 n_proc=1, is_swf=False, dir=None, testname='test_memmapping'):
+        super(TestMemMapping, self).__init__(testname)
+        self.n_samples = n_samples
+        self.n_features = n_features
+        self.memmap = memmap
+        self.n_proc = n_proc
+        self.is_swf = is_swf
+        self.dir = dir
 
-        print "X matrix file size =", os.path.getsize(X.filename), "bytes"
+    def test_memmapping(self):
+        ## 1) Building dataset
+        ## ============================================================
+        if self.memmap:
+            X = create_mmat(self.n_samples, self.n_features, dir=self.dir,
+                            n_proc=self.n_proc)
+            y = create_array(self.n_samples, [0, 1], dir=self.dir,
+                             n_proc=self.n_proc)
+            Xy = dict(X=X, y=y)
+        else:
+            X, y = datasets.make_classification(n_samples=self.n_samples,
+                                                n_features=self.n_features,
+                                                n_informative=2,
+                                                random_state=1)
+            Xy = dict(X=X, y=y)
+        ## 2) Building workflow
+        ## =======================================================
+        from sklearn.svm import SVC
+        from epac import CV, Methods
+        cv_svm_local = CV(Methods(*[SVC(kernel="linear"),
+                                    SVC(kernel="rbf")]), n_folds=3)
 
-        Xy = dict(X=X, y=y)
-    else:
+        cv_svm = None
+        if self.is_swf:
+            # Running on the cluster
+            from epac import SomaWorkflowEngine
+            mmap_mode = None
+            if self.memmap:
+                mmap_mode = "r+"
+            swf_engine = SomaWorkflowEngine(cv_svm_local,
+                                            num_processes=self.n_proc,
+                                            resource_id="jl237561@gabriel",
+                                            login="jl237561",
+#                                            remove_finished_wf=False,
+#                                            remove_local_tree=False,
+                                            mmap_mode=mmap_mode,
+                                            queue="Global_long")
 
-        X, y = datasets.make_classification(n_samples=n_samples,
-                                            n_features=n_features,
-                                            n_informative=2,
-                                            random_state=1)
+            cv_svm = swf_engine.run(**Xy)
+            time.sleep(2)
+            print ''
+            sum_memory = 0
+            max_time_cost = 0
+            for job_info in swf_engine.engine_info:
+                print "mem_cost=", job_info.mem_cost, \
+                      ", vmem_cost=", job_info.vmem_cost, \
+                      ", time_cost=", job_info.time_cost
+                sum_memory += job_info.mem_cost
+                if max_time_cost < job_info.time_cost:
+                    max_time_cost = job_info.time_cost
+            print "sum_memory =", sum_memory
+            print "max_time_cost =", max_time_cost
+        else:
+            # Running on the local machine
+            from epac import LocalEngine
+            local_engine = LocalEngine(cv_svm_local, num_processes=self.n_proc)
+            cv_svm = local_engine.run(**Xy)
 
-        Xy = dict(X=X, y=y)
-    ## 2) Building workflow
-    ## =======================================================
-    print " -> Pt2 : X and y created, building workflow"
-    from sklearn.svm import SVC
-    from epac import CV, Methods
-    cv_svm_local = CV(Methods(*[SVC(kernel="linear"),
-                                SVC(kernel="rbf")]),
-                      n_folds=3)
-    print " -> Pt3 : Workflow built, running"
+        cv_svm_reduce = cv_svm.reduce()
+        print "\n -> Reducing results"
+        print cv_svm_reduce
 
-    cv_svm = None
-    if is_swf:
-        # Running on the cluster
-        from epac import SomaWorkflowEngine
-        mmap_mode = None
-        if memmap:
-            mmap_mode = "r+"
-        swf_engine = SomaWorkflowEngine(cv_svm_local,
-                                        num_processes=n_proc,
-                                        resource_id="jl237561@gabriel",
-                                        login="jl237561",
-                                        # remove_finished_wf=False,
-                                        # remove_local_tree=False,
-                                        mmap_mode=mmap_mode,
-                                        queue="Global_long")
-        cv_svm = swf_engine.run(**Xy)
-    else:
-        # Running on the local machine
-        from epac import LocalEngine
-        local_engine = LocalEngine(cv_svm_local, num_processes=n_proc)
-        cv_svm = local_engine.run(**Xy)
-    print " -> Pt4 : Finished running reducing"
-    print cv_svm.reduce()
+        dirpath = "/tmp/tmp_save_tree/"
+        if not os.path.isdir(dirpath):
+            os.mkdir(dirpath)
 
-    print " -> Pt5 : Finished with", n_features, "features"
+        if self.n_proc == 1:
+            ## 4.1) Saving results on the disk for one process
+            ## ===================================================
+            store = StoreFs(dirpath=dirpath, clear=True)
+            cv_svm.save_tree(store=store)
 
-    ## 3) Removing files
-    ## =======================================================
-    if hasattr(X, "filename"):
-        os.remove(X.filename)
-    if hasattr(y, "filename"):
-        os.remove(y.filename)
+            with open("/tmp/tmp_save_results", 'w+') as filename:
+                pickle.dump(cv_svm_reduce, filename)
+
+        else:
+            ## 4.2) Compare results to the results for one process
+            ## ===================================================
+            store = StoreFs(dirpath=dirpath, clear=False)
+            cv_svm_one_proc = store.load()
+
+            with open("/tmp/tmp_save_results", 'r+') as filename:
+                cv_svm_reduce_one_proc = pickle.load(filename)
+
+            print "\nComparing %i proc with one proc" % self.n_proc
+            self.assertTrue(compare_two_node(cv_svm, cv_svm_one_proc))
+            self.assertTrue(isequal(cv_svm_reduce, cv_svm_reduce_one_proc))
 
 
 if __name__ == "__main__":
     args = sys.argv[1:]
     #print repr(args)
-    # args = [500, 70000, 'True', 2, 'True']
+#    args = [10, 70, 'True', 1, 'False', None]
     args[2] = (args[2] == 'True')
     args[4] = (args[4] == 'True')
-    func_memm_local(int(args[0]), int(args[1]), args[2],
-                    int(args[3]), args[4], args[5])
+    if len(args) == 5:
+        args.append(None)
+
+    suite = unittest.TestSuite()
+    suite.addTest(TestMemMapping(int(args[0]), int(args[1]),
+                                 args[2], int(args[3]), args[4], args[5]))
+#    suite.addTest(TestMemMapping(10, 80, True, 1, True, None))
+#    suite.addTest(TestMemMapping(10, 80, True, 2, True, None))
+
+    unittest.TextTestRunner().run(suite)
+#
+#
+#    optlist, args = getopt.getopt(sys.argv[1:], ["n_samples=",
+#                                                 "n_features=",
+#                                                 "memmap=",
+#                                                 "n_proc=",
+#                                                 "is_swf=",
+#                                                 "dir="])
+#
+#    for opt in optlist:
+#        if opt[0] == '--n_samples':
+#            n_samples = int(opt[1])
+#        elif opt[0] == '--n_features':
+#            n_features = int(opt[1])
+#        elif opt[0] == '--memmap':
+#            memmap = (opt[1] == 'True')
+#        elif opt[0] == '--n_proc':
+#            n_proc = int(opt[1])
+#        elif opt[0] == '--is_swf':
+#            is_swf = (opt[1] == 'True')
+#        elif opt[0] == '--dir':
+#            dir = opt[1]
