3fdb755d7933131391d52de20968ef433e5f3e32
jinpeng <mr.li.jinpeng@gmail.com>
2013-10-14 10:57:49 +0200

    ENH: add unit test for best search refit

--- epac/tests/test_cv_best_search_refit_parallel_reduce.py
+++ epac/tests/test_cv_best_search_refit_parallel_reduce.py
@@ -1,37 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 14 10:43:39 2013
+
+@author: jinpeng
+"""
+
+import unittest
 from sklearn.svm import SVC
 from sklearn.feature_selection import SelectKBest
 from epac import Pipe, CV, Methods
 from epac import CVBestSearchRefitParallel
 from epac import SomaWorkflowEngine
-from epac.configuration import conf
 from sklearn import datasets
+from epac.tests.utils import comp_2wf_reduce_res
+from epac.tests.utils import compare_two_node
 
-n_folds = 2
-n_folds_nested = 3
-k_values = [1, 2]
-C_values = [1, 2]
-n_samples = 500
-n_features = 10000
-n_cores = 2
-conf.MEMM_THRESHOLD = 50 * 5000 * 7
 
-X, y = datasets.make_classification(n_samples=n_samples,
-                                    n_features=n_features,
-                                    n_informative=5)
+class TestCVBestSearchRefitParallelReduce(unittest.TestCase):
+    def test_cv_best_search_refit_parallel(self):
+        n_folds = 2
+        n_folds_nested = 3
+        k_values = [1, 2]
+        C_values = [1, 2]
+        n_samples = 500
+        n_features = 10000
+        n_cores = 2
+        X, y = datasets.make_classification(n_samples=n_samples,
+                                            n_features=n_features,
+                                            n_informative=5)
+        # epac workflow for paralle computing
+        pipelines = Methods(*[Pipe(SelectKBest(k=k),
+                              Methods(*[SVC(kernel="linear", C=C)
+                              for C in C_values]))
+                              for k in k_values])
+        pipeline = CVBestSearchRefitParallel(pipelines,
+                                             n_folds=n_folds_nested)
+        wf = CV(pipeline, n_folds=n_folds)
 
-pipelines = Methods(*[Pipe(SelectKBest(k=k),
-                      Methods(*[SVC(kernel="linear", C=C)
-                      for C in C_values]))
-                      for k in k_values])
+        sfw_engine = SomaWorkflowEngine(tree_root=wf,
+                                        num_processes=n_cores,
+                                        remove_finished_wf=False,
+                                        remove_local_tree=False)
+        sfw_engine_wf = sfw_engine.run(X=X, y=y)
 
-pipeline = CVBestSearchRefitParallel(pipelines,
-                                     n_folds=n_folds_nested)
-wf = CV(pipeline, n_folds=n_folds)
+        # epac workflow for normal node computing
+        pipelines2 = Methods(*[Pipe(SelectKBest(k=k),
+                              Methods(*[SVC(kernel="linear", C=C)
+                              for C in C_values]))
+                              for k in k_values])
+        pipeline2 = CVBestSearchRefitParallel(pipelines2,
+                                             n_folds=n_folds_nested)
+        wf2 = CV(pipeline2, n_folds=n_folds)
+        wf2.run(X=X, y=y)
 
-sfw_engine = SomaWorkflowEngine(tree_root=wf,
-                                num_processes=n_cores,
-                                remove_finished_wf=False,
-                                remove_local_tree=False)
+        self.assertTrue(compare_two_node(sfw_engine_wf, wf2))
+        self.assertTrue(comp_2wf_reduce_res(sfw_engine_wf, wf2))
 
-sfw_engine_wf = sfw_engine.run(X=X, y=y)
-print sfw_engine_wf.reduce()
+if __name__ == '__main__':
+    unittest.main()
